7da7c15289f93d0c6e1412d7c4821a1b81d58f13
vansteve911 <vansteve911@gmail.com>
2016-07-10 12:15:05 +0800

    redis cache

--- config.js
+++ config.js
@@ -14,5 +14,26 @@ module.exports = {
     port: process.env.PGPORT || 5432, //env var: PGPORT
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 1000, // how long a client is allowed to remain idle before being closed
+  },
+  redis: {
+    host: '127.0.0.1',
+    port: 6379,
+    idleTimeout: 3000, // custom config
+    retry_strategy: function(options) {
+      if (options.error.code === 'ECONNREFUSED') {
+        // End reconnecting on a specific error and flush all commands with a individual error
+        return new Error('The server refused the connection');
+      }
+      if (options.total_retry_time > 1000 * 60 * 60) {
+        // End reconnecting after a specific timeout and flush all commands with a individual error
+        return new Error('Retry time exhausted');
+      }
+      if (options.times_connected > 10) {
+        // End reconnecting with built in error
+        return undefined;
+      }
+      // reconnect after
+      return Math.max(options.attempt * 100, 3000);
+    }
   }
 };
--- storage/cache.js
+++ storage/cache.js
@@ -1,4 +1,70 @@
 'use strict';
-const redis = require('redis');
-
+const redis = require('redis'),
+  config = require('../config');
 // TODO
+
+function RedisCache() {}
+
+RedisCache.prototype.activateClient = function() {
+  let self = this;
+  if (!self.client || !self.client.connected) {
+    self.client = redis.createClient(config.redis);
+    self.client.on('error', function(err) {
+      console.error('client error!', err.stack);
+    });
+    self.client.on('end', function() {
+      console.log('client quit');
+    });
+    self.closeClientTimer = setTimeout(function() {
+      self.client.quit();
+    }, config.redis.idleTimeout);
+  } else {
+    // activate client
+    if (self.closeClientTimer) {
+      // cancel close timer
+      clearTimeout(self.closeClientTimer);
+      self.closeClientTimer = setTimeout(function() {
+        self.client.quit();
+      }, config.redis.idleTimeout);
+    }
+  }
+}
+
+RedisCache.prototype.get = function(args) {
+  let self = this, key;
+  return new Promise(function(resolve, reject) {
+    self.activateClient();
+    if (args && (key = args.key)) {
+      self.client.get(key, function(err, res) {
+        if (err) {
+          console.error('failed to get key: ' + key, err.message, err.stack);
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    } else {
+      reject(new Error('empty args'));
+    }
+  });
+}
+RedisCache.prototype.set = function(args) {
+  let self = this, key, value;
+  return new Promise(function(resolve, reject) {
+    self.activateClient();
+    if (args && (key = args.key) && (value = args.value)) {
+      self.client.set(key, value, function(err, res) {
+        if (err) {
+          console.error('failed to set, key: ' + key + ', value: ' + value, err.message, err.stack);
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    } else {
+      reject(new Error('empty args'));
+    }
+  });
+}
+
+module.exports = RedisCache;
